e429e36d7e66761837a0a5b6f359e3410f0a002f
Maya Berg <mayaberg31@gmail.com>
2024-02-07 11:04:36 +0200

    use tmp_path for output files

--- tests/test_vcf_writer.py
+++ tests/test_vcf_writer.py
@@ -10,8 +10,8 @@ class TestVcfWriter:
     @pytest.mark.parametrize("input_file, expected_output_file", (
             ('tests/samples/sample1.vcf', 'tests/samples/sample1.vcf'),
     ))
-    def test_write_w_variants_headers(self, input_file, expected_output_file):
-        output_path = Path('/tmp/output.vcf')
+    def test_write_w_variants_headers(self, input_file, expected_output_file, tmp_path):
+        output_path = tmp_path / "output.vcf"
         with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
             writer.write(variants=reader, headers=reader.headers)
         assert output_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
@@ -19,8 +19,8 @@ class TestVcfWriter:
     @pytest.mark.parametrize("input_file, expected_output_file", (
             ('tests/samples/sample1.vcf', 'tests/samples/sample1.no_headers.vcf'),
     ))
-    def test_write_w_variants(self, input_file, expected_output_file):
-        output_path = Path('/tmp/output.vcf')
+    def test_write_w_variants(self, input_file, expected_output_file, tmp_path):
+        output_path = tmp_path / "output.vcf"
         with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
             writer.write(variants=reader)
 
@@ -33,8 +33,8 @@ class TestVcfWriter:
     @pytest.mark.parametrize("input_file, expected_output_file", (
             ('tests/samples/sample1.vcf', 'tests/samples/sample1.no_variants.vcf'),
     ))
-    def test_write_w_headers(self, input_file, expected_output_file):
-        output_path = Path('/tmp/output.vcf')
+    def test_write_w_headers(self, input_file, expected_output_file, tmp_path):
+        output_path = tmp_path / "output.vcf"
         with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
             writer.write(headers=reader.headers)
         assert output_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
@@ -58,17 +58,17 @@ class TestVcfWriter:
             ('.vcf.bgz', 'tests/samples/sample1.tabixed.vcf.bgz', True, 'tests/samples/sample1.vcf'),
     ))
     def test_write_filetypes(self, tmp_path, output_suffix, input_file, write_headers, expected_output_file):
-        output_path = tmp_path / ('output' + output_suffix)
-        with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
+        output_file_path = tmp_path / ('output' + output_suffix)
+        with VcfReader(input_file) as reader, VcfWriter(output_file_path) as writer:
             writer.write(headers=reader.headers, variants=reader)
-        assert open_file(output_path).read(9999999).encode().strip() == Path(expected_output_file).read_bytes().strip()
+        assert open_file(output_file_path).read(9999999).encode().strip() == Path(expected_output_file).read_bytes().strip()
 
     @pytest.mark.parametrize("input_file, expected_output_file", (
             ('tests/samples/sample1.vcf', 'tests/samples/expected_info_headers_before_last_line.vcf'),
     ))
-    def test_write_non_sorted_headers_at_the_end(self, input_file, expected_output_file):
-        output_path = Path('/tmp/output.vcf')
-        with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
+    def test_write_non_sorted_headers_at_the_end(self, input_file, expected_output_file, tmp_path):
+        output_file_path = tmp_path / "output.vcf"
+        with VcfReader(input_file) as reader, VcfWriter(output_file_path) as writer:
             new_headers = [
                 '##INFO=<ID=tier1_rank,Number=1,Type=Integer,Description="AI rank for tier 1">',
                 '##INFO=<ID=tier2_rank,Number=1,Type=Integer,Description="AI rank for tier 2">'
@@ -76,14 +76,14 @@ class TestVcfWriter:
             # add headers at the end of the headers list and write to file
             headers = reader.headers + new_headers
             writer.write(variants=reader, headers=headers)
-        assert output_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
+        assert output_file_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
 
     @pytest.mark.parametrize("input_file, expected_output_file", (
             ('tests/samples/sample1.vcf', 'tests/samples/expected_info_headers_after_first_line.vcf'),
     ))
-    def test_write_non_sorted_headers_at_the_beginning(self, input_file, expected_output_file):
-        output_path = Path('/tmp/output.vcf')
-        with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
+    def test_write_non_sorted_headers_at_the_beginning(self, input_file, expected_output_file, tmp_path):
+        output_file_path = tmp_path / "output.vcf"
+        with VcfReader(input_file) as reader, VcfWriter(output_file_path) as writer:
             new_headers = [
                 '##INFO=<ID=tier1_rank,Number=1,Type=Integer,Description="AI rank for tier 1">',
                 '##INFO=<ID=tier2_rank,Number=1,Type=Integer,Description="AI rank for tier 2">'
@@ -91,14 +91,14 @@ class TestVcfWriter:
             # add headers at the beginning of the headers list and write to file
             headers = new_headers + reader.headers
             writer.write(variants=reader, headers=headers)
-        assert output_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
+        assert output_file_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
 
     @pytest.mark.parametrize("input_file, expected_output_file", (
             ('tests/samples/sample1.vcf', 'tests/samples/expected_info_headers_before_last_line.vcf'),
     ))
-    def test_write_non_sorted_headers_at_the_end(self, input_file, expected_output_file):
-        output_path = Path('/tmp/output.vcf')
-        with VcfReader(input_file) as reader, VcfWriter(output_path) as writer:
+    def test_write_non_sorted_headers_at_the_end(self, input_file, expected_output_file, tmp_path):
+        output_file_path = tmp_path / "output.vcf"
+        with VcfReader(input_file) as reader, VcfWriter(output_file_path) as writer:
             new_headers = [
                 '##INFO=<ID=tier1_rank,Number=1,Type=Integer,Description="AI rank for tier 1">',
                 '##INFO=<ID=tier2_rank,Number=1,Type=Integer,Description="AI rank for tier 2">'
@@ -106,4 +106,4 @@ class TestVcfWriter:
             # add headers at the end of the headers list and write to file
             headers = reader.headers + new_headers
             writer.write(variants=reader, headers=headers)
-        assert output_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
+        assert output_file_path.read_bytes().strip() == Path(expected_output_file).read_bytes().strip()
